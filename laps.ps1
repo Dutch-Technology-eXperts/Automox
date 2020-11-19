5f1f7238595f888523f90b69697e9a903b5fafca
TWFpa2Vs <maikel.roolvink@outlook.com>
2020-11-19 10:54:32 +0100

    Worklet to create the local user for laps and install agent

---
+++ laps.ps1
@@ -0,0 +1,29 @@
+### Variables ###
+$username = "" #### Enter username you want to create ###
+$password = "" ### Enter initial password ###
+### Variables ###
+
+$User=gwmi -class Win32_UserAccount | Where {$_.Name -eq $username}
+
+if (-Not $User)
+{ 
+[void](net user /add $username $password)
+[void](net localgroup administrators $username /add)
+    if ($? -eq "True")
+        { Write-Output "User successfully created." }
+    else {Write-Output "Failed to create user!" }
+}
+Else {Write-Output "User already exists." }
+
+$Installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName -Match "Local Administrator Password Solution" })
+
+If(-Not $Installed) {
+    [void](Start-Process -FilePath 'msiexec.exe' -ArgumentList ('/qn', '/i', '"LAPS.x64.msi"') -Wait -Passthru)
+    
+    if ($? -eq "True")
+        { Write-Output "LAPS client successfully installed." }
+    else {Write-Output "Failed to install LAPS client!" }
+    } 
+else {
+    Write-Output "LAPS client already installed."
+    }
